3d028c2a1c003bc8a03a2ffa8e03baff29173b3d
joatjoe <joelninr@gmail.com>
2023-04-22 10:35:07 -0400

    Adding on Prep2 the grade's system.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage? ");
+        string percentage_s = Console.ReadLine();
+        int percentage_i = int.Parse(percentage_s);
+
+        string letter = "";
+
+        if (percentage_i >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage_i >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (percentage_i >= 70)
+        {
+            letter = "C";
+        }
+        
+        else if (percentage_i >= 60)
+        {
+            letter = "D";
+        }
+        
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percentage_i >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        
+        else
+        {
+            Console.WriteLine("Sorry, better luck next time!");
+        }
+
     }
 }
