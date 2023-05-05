42877bf83f2ee52026ea66741807b92b132d8bbc
joatjoe <joelninr@gmail.com>
2023-05-05 00:37:12 -0400

    Adding changes to Learning02.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job01 = new Job();
+        job01._jobTitle = "Software Engineer";
+        job01._company = "Microsoft";
+        job01._startYear = 2019;
+        job01._endYear = 2022;
+
+        Job job02 = new Job();
+        job02._jobTitle = "Manager";
+        job02._company = "Apple";
+        job02._startYear = 2022;
+        job02._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._personName = "Allison Rose";
+
+        resume._jobs.Add(job01);
+        resume._jobs.Add(job02);
+
+        resume.Display();
+
     }
 }
