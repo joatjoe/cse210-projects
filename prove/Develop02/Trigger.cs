6c09fe5452081f9717f1c9fabf7e002b3f6a88cb
joatjoe <joelninr@gmail.com>
2023-05-15 03:16:08 -0400

    Questions added.

--- prove/Develop02/Trigger.cs
+++ prove/Develop02/Trigger.cs
@@ -9,7 +9,17 @@ public class Trigger
             "What was the strongest emotion I felt today?",
             "If I had one thing I could do over today, what would it be?",
             "Was the food good today?",
-            "Where did I go today?"
+            "Where did I go today?",
+            "What was the most significant event(s) of the day?",
+            "In what way was this day different from other days?",
+            "Did I have any significant conversations?",
+            "What did I read today? Did it say anything special to me?",
+            "How did I feel during the day? What were the high and low points?",
+            "Was I worrying about anything today?",
+            "What gave me particular happiness or joy today?",
+            "What did I accomplish today?",
+            "Did I fail at anything and what lesson did I learn?",
+            "Did I gain any insights into myself or someone else?"
     };
 
     public string GetPrompt()
